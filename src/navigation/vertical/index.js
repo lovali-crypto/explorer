2560ff0e90683fcf12b54d94bf9d4b2fee225e55
balda <marcotorello@gmail.com>
2022-02-22 19:43:01 +0100

    fix indent

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,94 +1,95 @@
 import store from '@/store'
 import { isTestnet } from '../../libs/utils'
 
-const modules = [{
-        scope: 'normal',
-        title: 'summary',
-        route: 'info',
-    },
-    {
-        scope: 'normal',
-        title: 'blocks',
-        route: 'blocks',
-    },
-    {
-        scope: 'normal',
-        title: 'staking',
-        route: 'staking',
-    },
-    {
-        scope: 'normal',
-        title: 'governance',
-        route: 'governance',
-        exclude: 'emoney',
-    },
-    {
-        scope: 'normal',
-        title: 'uptime',
-        route: 'uptime',
-    },
-    {
-        scope: 'cos-mos',
-        title: 'gravity',
-        route: 'gravity',
-    },
-    {
-        scope: 'osmosis',
-        title: 'trade',
-        route: 'osmosis-trade',
-    },
+const modules = [
+  {
+    scope: 'normal',
+    title: 'summary',
+    route: 'info',
+  },
+  {
+    scope: 'normal',
+    title: 'blocks',
+    route: 'blocks',
+  },
+  {
+    scope: 'normal',
+    title: 'staking',
+    route: 'staking',
+  },
+  {
+    scope: 'normal',
+    title: 'governance',
+    route: 'governance',
+    exclude: 'emoney',
+  },
+  {
+    scope: 'normal',
+    title: 'uptime',
+    route: 'uptime',
+  },
+  {
+    scope: 'cos-mos',
+    title: 'gravity',
+    route: 'gravity',
+  },
+  {
+    scope: 'osmosis',
+    title: 'trade',
+    route: 'osmosis-trade',
+  },
 ]
 
 function processMenu() {
-    const chainMenus = [
-        // {
-        //   title: 'Home',
-        //   route: 'home',
-        //   icon: 'HomeIcon',
-        // },
-        {
-            header: 'blockchains',
-        },
-    ]
-    Object.keys(store.state.chains.config).forEach(chain => {
-        const menu = {
-            title: chain,
-            icon: store.state.chains.config[chain].logo,
+  const chainMenus = [
+    // {
+    //   title: 'Home',
+    //   route: 'home',
+    //   icon: 'HomeIcon',
+    // },
+    {
+      header: 'blockchains',
+    },
+  ]
+  Object.keys(store.state.chains.config).forEach(chain => {
+    const menu = {
+      title: chain,
+      icon: store.state.chains.config[chain].logo,
+    }
+    const { excludes } = store.state.chains.config[chain]
+    const children = []
+    modules.forEach(m => {
+      if (excludes === undefined || excludes.indexOf(m.route) === -1) {
+        if (m.scope.match('normal') || m.scope.match(chain)) {
+          children.push({
+          // header: `item-${chain}-${m.route}`,
+            title: m.title,
+            route: { name: m.route, params: { chain } },
+          })
         }
-        const { excludes } = store.state.chains.config[chain]
-        const children = []
-        modules.forEach(m => {
-            if (excludes === undefined || excludes.indexOf(m.route) === -1) {
-                if (m.scope.match('normal') || m.scope.match(chain)) {
-                    children.push({
-                        // header: `item-${chain}-${m.route}`,
-                        title: m.title,
-                        route: { name: m.route, params: { chain } },
-                    })
-                }
-            }
-        })
-        menu.children = children
-        chainMenus.push(menu)
+      }
     })
-    chainMenus.push({ header: 'LINKS' })
-        /*chainMenus.push({
-          title: 'Discord',
-          href: 'https://discord.gg/CmjYVSr6GW',
-          icon: 'EyeIcon',
-        }) */
-    chainMenus.push({
-        title: 'Twitter',
-        href: 'https://twitter.com/lovalicrypto',
-        icon: 'TwitterIcon',
-    })
-    chainMenus.push({
-        title: 'Github',
-        href: 'https://github.com/lovali-crypto/explorer',
-        icon: 'GithubIcon',
-    })
+    menu.children = children
+    chainMenus.push(menu)
+  })
+  chainMenus.push({ header: 'LINKS' })
+  /* chainMenus.push({
+    title: 'Discord',
+    href: 'https://discord.gg/CmjYVSr6GW',
+    icon: 'EyeIcon',
+  }) */
+  chainMenus.push({
+    title: 'Twitter',
+    href: 'https://twitter.com/lovalicrypto',
+    icon: 'TwitterIcon',
+  })
+  chainMenus.push({
+    title: 'Github',
+    href: 'https://github.com/lovali-crypto/explorer',
+    icon: 'GithubIcon',
+  })
 
-    return chainMenus
+  return chainMenus
 }
 
 export default processMenu()
